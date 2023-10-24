39cbe799876727396ae94f15fbf039047af5ec6e
Launyx <lunyx.castelhano@gmail.com>
2023-10-24 20:23:43 -0300

    Bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,80 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece{
+
+    public Bishop(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);    // Instantiate a new position p, to use as a checker for possible moves for the Rook
+
+        // NW
+        p.setValues(this.position.getRow() - 1, this.position.getColumn() -1);   // set new values for the position "p", in this case, to one row above
+
+        // loop to check the matrix of the board
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){    // While the position "p" exists in the board AND there is no piece in that position
+            mat[p.getRow()][p.getColumn()] = true;                              // Sets the position as true, making it a possible move to the Rook
+            p.setValues(p.getRow() -1, p.getColumn() -1);                                          // sets a new row to the position "p", until it finds the end of the board or a piece
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){    // checks if the position "p" stopped in the last loop exists AND if its an opponent piece
+            mat[p.getRow()][p.getColumn()] = true;                   // if the position exists and it is an enemy, marks the position in the matrix as true
+        }
+
+        // NE
+        p.setValues(this.position.getRow() -1, this.position.getColumn() +1);   // set new values for the position "p", in this case, to one column to the left
+
+        // loop to check the matrix of the board
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){    // While the position "p" exists in the board AND there is no piece in that position
+            mat[p.getRow()][p.getColumn()] = true;                              // Sets the position as true, making it a possible move to the Rook
+            p.setValues(p.getRow() -1, p.getColumn() +1);                                     // sets a new column to the position "p", until it finds the end of the board or a piece
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){    // checks if the position "p" stopped in the last loop exists AND if its an opponent piece
+            mat[p.getRow()][p.getColumn()] = true;                   // if the position exists and it is an enemy, marks the position as true
+        }
+        
+        // SE
+        p.setValues(this.position.getRow() +1, this.position.getColumn() +1);   // set new values for the position "p", in this case, to one column to the right
+
+        // loop to check the matrix of the board
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){    // While the position "p" exists in the board AND there is no piece in that position
+            mat[p.getRow()][p.getColumn()] = true;                              // Sets the position as true, making it a possible move to the Rook
+            p.setValues(p.getRow() +1, p.getColumn() +1);;                                     // sets a new column to the position "p", until it finds the end of the board or a piece
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){    // checks if the position "p" stopped in the last loop exists AND if its an opponent piece
+            mat[p.getRow()][p.getColumn()] = true;                   // if the position exists and it is an enemy, marks the position as true
+        }
+
+        // SW
+        p.setValues(this.position.getRow() + 1, this.position.getColumn() -1);   // set new values for the position "p", in this case, to one column to the right
+
+        // loop to check the matrix of the board
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){    // While the position "p" exists in the board AND there is no piece in that position
+            mat[p.getRow()][p.getColumn()] = true;                              // Sets the position as true, making it a possible move to the Rook
+            p.setValues(p.getRow() +1, p.getColumn() -1);;                                           // sets a new row to the position "p", until it finds the end of the board or a piece
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){    // checks if the position "p" stopped in the last loop exists AND if its an opponent piece
+            mat[p.getRow()][p.getColumn()] = true;                   // if the position exists and it has an opponent piece, marks the position as true so the piece can be caputered
+        }
+
+        return mat;
+    }
+
+    @Override
+    public String toString(){
+        return "B";
+    }
+    
+}
